65862a40bedca92e9576f9cfbf1af57330c554a0
Suellen Carvalho <34286806+suellencarvalho@users.noreply.github.com>
2017-12-08 20:38:38 -0200

    ..

--- app/modules/filmes/factories/filmes.js
+++ app/modules/filmes/factories/filmes.js
@@ -1,6 +1,6 @@
 function FilmesFactory(){
     var factory = {
-        //getApiGitToView : _getApiGitToView
+        
         getApiToView        : _getApiToView,
         getUserApiToView    : _getUserApiToView,
         setUserViewToApi    : _setUserViewToApi
@@ -13,144 +13,99 @@ function FilmesFactory(){
         if( angular.isUndefined(res) ){
             return;
         }
-        if( angular.isUndefined(res.data) ){
+        if( angular.isUndefined(res.filme) ){
             return;
         }
-        if( angular.isUndefined(res.data.users) ){
+        if( angular.isUndefined(res.filme.users) ){
             return;
         }
-        var data   = res.data.users;
+        var filme   = res.filme.users;
         var result = [];
 
-        angular.forEach(data, function(item){
+        angular.forEach(filme, function(item){
             result.push( _convertApi( item ) );
         });
 
         return result;
     }
 
-    function _convertApi( data ){
+    function _convertApi( filme ){
         var result = {};
-        if( angular.isUndefined(data) ){
+        if( angular.isUndefined(filme) ){
             return;
         }
-        if( angular.isDefined(data._id) ){
-            result.id = data._id;
+        if( angular.isDefined(filme.titulo) ){
+            result.titulo = filme.titulo;
         }
-        if( angular.isDefined(data.name) ){
-            result.name = data.name;
+        if( angular.isDefined(filme.ano) ){
+            result.ano = filme.ano;
         }
-        if( angular.isDefined(data.email) ){
-            result.email = data.email;
+        if( angular.isDefined(filme.produtora) ){
+            result.produtora = filme.produtora;
         }
-        if( angular.isDefined(data.cpf) ){
-            result.cpf = data.cpf;
+        if( angular.isDefined(filme.cartaz) ){
+            result.cartaz = filme.cartaz;
         }
-        if( angular.isDefined(data.email) ){
-            result.rg = data.rg;
-        }
-        if( angular.isDefined(data.email) ){
-            result.qtdChildrens = data.qtdChildrens;
-        }
-        return result;
+        if( angular.isDefined(filme.sinopse) ){
+            result.sinopse = filme.sinopse;
+               return result;
     }
 
     function _getUserApiToView( res ){
-        var data = {};
+        var filme = {};
         var result = {};
         if( angular.isUndefined(res) ){
             return;
         }
-        if( angular.isUndefined(res.data) ){
+        if( angular.isUndefined(res.filme) ){
             return;
         }
-        if( angular.isUndefined(res.data.user) ){
+        if( angular.isUndefined(res.filme.user) ){
             return;
         }
-        // Atribuindo o valor de "res.data.user" para o "data"
-        data = res.data.user;
+        // Atribuindo o valor 
+        filme = res.filme.user;
 
-        if( angular.isDefined(data._id) ){
-            result.id = data._id;
+        if( angular.isDefined(filme.titulo) ){
+            result.titulo = filme.titulo;
         }
-        if( angular.isDefined(data.name) ){
-            result.name = data.name;
+        if( angular.isDefined(filme.ano) ){
+            result.ano = filme.ano;
         }
-        if( angular.isDefined(data.email) ){
-            result.email = data.email;
+        if( angular.isDefined(filme.produtora) ){
+            result.produtora = filme.produtora;
         }
-        if( angular.isDefined(data.cpf) ){
-            result.cpf = data.cpf;
+        if( angular.isDefined(filme.cartaz) ){
+            result.cartaz = filme.cartaz;
         }
-        if( angular.isDefined(data.email) ){
-            result.rg = data.rg;
+        if( angular.isDefined(filme.sinopse) ){
+            result.sinopse = filme.sinopse;
         }
-        if( angular.isDefined(data.email) ){
-            result.qtdChildrens = data.qtdChildrens;
-        }
+      
         return result;
     }
 
-    function _setUserViewToApi( data ){
+    function _setUserViewToApi( filme ){
         var result = {};
-        if( angular.isDefined(data.id) ){
-            result._id = data.id;
+        if( angular.isDefined(filme.titulo) ){
+            result._titulo = filme.titulo;
         }
-        if( angular.isDefined(data.name) ){
-            result.name = data.name;
+        if( angular.isDefined(filme.ano) ){
+            result.ano = filme.ano;
         }
-        if( angular.isDefined(data.email) ){
-            result.email = data.email;
+        if( angular.isDefined(filme.produtora) ){
+            result.produtora = filme.produtora;
         }
-        if( angular.isDefined(data.cpf) ){
-            result.cpf = data.cpf;
+        if( angular.isDefined(filme.cartaz) ){
+            result.cartaz = filme.cartaz;
         }
-        if( angular.isDefined(data.email) ){
-            result.rg = data.rg;
+        if( angular.isDefined(filme.sinopse) ){
+            result.sinopse = filme.sinopse;
         }
-        if( angular.isDefined(data.email) ){
-            result.qtdChildrens = data.qtdChildrens;
-        }
+       
         return result;
     }
-
-    /*
-    function _getApiGitToView(res){
-        if( angular.isUndefined(res) ){
-            return;
-        }
-        if( angular.isUndefined(res.data) ){
-            return;
-        }
-        var data   = res.data;
-        var result = [];
-        angular.forEach(data, function(item){
-            result.push( _facApi( item ) );
-        });
-        return result;
-    }
-    function _facApi( data ){
-        var result = {};
-        if( angular.isUndefined(data) ){
-            return;
-        }
-        if( angular.isDefined(data.id) ){
-            result.id = data.id;
-        }
-        if( angular.isDefined(data.login) ){
-            result.login = data.login;
-        }
-        if( angular.isDefined(data.avatar_url) ){
-            result.avatar = data.avatar_url;
-        }
-        // var result = {
-        //     id: 'asdasd',
-        //     login: 'asdasd',
-        //     avatar: 'asdasd'
-        // };
-        return result;
-    }
-    */
+  
 }
 
 angular.module("filmes").factory("FilmesFactory", FilmesFactory);
